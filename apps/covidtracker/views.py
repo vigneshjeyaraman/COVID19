ced3a2b19e5dc9830dd3e52f712988ef14280492
Vignesh-Jeyaraman <vignesh.jeyaraman@algoworks.com>
2020-09-08 20:20:14 +0530

    added last_updated

--- apps/covidtracker/views.py
+++ apps/covidtracker/views.py
@@ -1,18 +1,21 @@
-from django.shortcuts import render
+from io import BytesIO
+import base64
+from datetime import datetime
 from covid import Covid
 import numpy as np
 import matplotlib.pyplot as plt
 
 import matplotlib
 matplotlib.use("Agg")
-from io import BytesIO
-import base64
+
+from django.shortcuts import render
 
 
 # Create your views here.
 covid = Covid()
 def home(request):
     data = covid.get_data()
+    last_updated = datetime.utcfromtimestamp(data[0]['last_update']/1000).strftime('%Y-%m-%d %H:%M:%S')
     countries = [x['country'] for x in data]
     active = [x['active'] for x in data]
     plt.xlabel('Country')
@@ -26,13 +29,14 @@ def home(request):
     plt.savefig(buf, format='png', dpi=300)
     image_base64 = base64.b64encode(buf.getvalue()).decode('utf-8').replace('\n', '')
     buf.close()
-    return render(request, 'home.html', {"image_base64": image_base64})
+    return render(request, 'home.html', {"image_base64": image_base64, "last_updated": last_updated})
 
 def searchcountry(request):
     countries = covid.list_countries()
     if request.method == 'POST':
         country_stats = covid.get_status_by_country_id(request.POST.get('country'))
-        return render(request, 'searchforyourcountry.html', {'countries': countries, "stats": country_stats, "selected": country_stats['country']})
+        last_updated = datetime.utcfromtimestamp(country_stats['last_update']/1000).strftime('%Y-%m-%d %H:%M:%S')
+        return render(request, 'searchforyourcountry.html', {'countries': countries, "stats": country_stats, "selected": country_stats['country'], "last_updated": last_updated})
     return render(request, 'searchforyourcountry.html', {'countries': countries})
 
 def overview(request):
